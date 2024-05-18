14f5d1e4a53d41bb45775f25b21cfe381ac95a45
FlamingFury00 <manuel.digiovanni13.mdg@gmail.com>
2024-05-18 17:13:18 +0200

    Update

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ class Bot(GoslingAgent):
         if self.intent is not None:
             return
 
-        # Cooperation
-        if len(self.friends) > 0:
+        # 2v2
+        if len(self.friends) == 1:
             if self.kickoff_flag and is_closest_kickoff(self, self.me):
                 self.set_intent(kickoff(self.me.location.x))
                 return
@@ -111,6 +111,110 @@ class Bot(GoslingAgent):
                 should_attack(self)
                 and not friends_ahead_of_ball(self)
                 and friends_attacking(self) >= 1
+                and (self.ball.latest_touched_team != self.me.team)
+            ):
+                self.set_intent(go_centre())
+                return
+
+            # Attack
+            if should_attack(self):
+                best_shot = find_best_shot(self, self.get_closest_opponent())
+                if best_shot is not None:
+                    self.set_intent(best_shot)
+                    return
+
+            # Dribbling
+            if self.me.boost > 30 and self.is_close_to_ball(200):
+                self.set_intent(dribble(self.foe_goal.location))
+                return
+
+            # Team play
+            if friendly_cars_in_front_of_goal(self) and is_ball_centering(self):
+                self.set_intent(center_ball(self.foe_goal.location))
+                return
+
+            # Defence
+            # if is_ball_going_towards_goal(self) and is_second_closest(self):
+            best_save = find_best_save(self, self.get_closest_opponent())
+            if best_save is not None:
+                self.set_intent(best_save)
+                return
+
+            # Demo and avoid demo
+            # if self.me.boost > 50:
+            #     closest_opponent = self.get_closest_opponent()
+            #     if closest_opponent is not None and self.is_close_to_opponent(
+            #         closest_opponent
+            #     ):
+            #         self.set_intent(demo(closest_opponent))
+            #         return
+            # else:
+            #     demo_intent = detect_demo(self)
+            #     if demo_intent[1] is not None and not demo_intent[0]:
+            #         self.set_intent(avoid_demo(self.get_closest_opponent()))
+
+            if are_no_bots_back(self) and is_second_closest(self):
+                return self.set_intent(goto(self.friend_goal.location))
+        # 3v3
+        elif len(self.friends) == 2:
+            if self.kickoff_flag and is_closest_kickoff(self, self.me):
+                self.set_intent(kickoff(self.me.location.x))
+                return
+
+            if self.kickoff_flag and is_second_closest_kickof(self):
+                self.set_intent(kickoff2())
+                return
+
+            # if is_ball_going_towards_goal(self) and is_closest(self, self.me):
+            #     self.set_intent(short_shot(self.foe_goal.location))
+            #     return
+
+            # Rotation and positioning
+            if self.should_rotate():
+                desired_zone = zone_5_positioning(self)
+                if desired_zone is not None:
+                    return self.set_intent(goto(desired_zone[0]))
+
+            if self.is_in_front_of_ball() and are_no_bots_back(self):
+                return self.set_intent(
+                    goto(self.friend_goal.location, self.ball.velocity)
+                )
+
+            # Boost grabbing
+            if self.me.boost < 30 and is_second_closest(self):
+                target_boost = self.get_best_boost()
+                if target_boost is not None:
+                    self.set_intent(steal_boost(target_boost))
+                    return
+
+            # if (
+            #     should_attack(self)
+            #     and friends_attacking(self) >= 1
+            #     and self.get_closest_teammate().location.distance(self.ball.location)
+            #     < self.me.location.distance(self.ball.location)
+            #     and not friends_ahead_of_ball(self)
+            # ):
+            #     return self.set_intent(go_centre())
+
+            if (
+                should_attack(self)
+                and not friends_ahead_of_ball(self)
+                and friends_attacking(self) == 2
+                and (self.ball.latest_touched_team != self.me.team)
+                and self.me.boost >= 30
+            ):
+                self.set_intent(demo(self.get_opponent_closest_to_ball()))
+                return
+
+            demo_intent = detect_demo(self)
+            if demo_intent[1] is not None:
+                self.set_intent(avoid_demo(self.get_closest_opponent()))
+                return
+
+            if (
+                should_attack(self)
+                and not friends_ahead_of_ball(self)
+                and friends_attacking(self) == 2
                 and (self.ball.latest_touched_team != self.me.team)
             ):
                 self.set_intent(go_centre())
